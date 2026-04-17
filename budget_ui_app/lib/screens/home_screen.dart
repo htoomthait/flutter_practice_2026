d0598a7303e6b1f4461a0fa42dd62d281c87c540
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-04-17 15:06:15 +0630

    home screen initiated for budget ui app

---
+++ budget_ui_app/lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text("Budget UI App", style: TextStyle(color: Colors.white, fontSize: 24.0, fontWeight: FontWeight.w600))
+      ),
+    );
+  }
+}
