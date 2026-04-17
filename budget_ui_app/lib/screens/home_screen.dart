0d0e0f40b6799061db3a7c301680b4f9fede362a
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-04-17 15:35:40 +0630

    sliver app bar and silver list lesson done

--- budget_ui_app/lib/screens/home_screen.dart
+++ budget_ui_app/lib/screens/home_screen.dart
@@ -11,10 +11,45 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Theme.of(context).primaryColor,
-        title: Text("Budget UI App", style: TextStyle(color: Colors.white, fontSize: 24.0, fontWeight: FontWeight.w600))
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            forceElevated: true,
+            pinned: true,
+            floating: false,
+            expandedHeight: 100.0,
+            backgroundColor: Theme.of(context).primaryColor,
+            centerTitle: true,
+            leading: IconButton(icon: Icon(Icons.settings), iconSize: 30.0, onPressed: (){},),
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text(
+                "Budget UI App",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            actions: <Widget>[
+              IconButton(icon: Icon(Icons.add), iconSize: 30.0, onPressed: (){},)
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index){
+                return Container(
+                  margin: EdgeInsets.all(10.0),
+                  height: 100.0,
+                  color: Colors.red
+                );
+              }
+              ,
+              childCount: 10,
+            ),
+          ),
+        ],
       ),
     );
   }
