658c98717e3ed702c9317aeee6b7b0c2f9fc862d
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-04-18 20:39:38 +0630

    category list card

--- budget_ui_app/lib/screens/home_screen.dart
+++ budget_ui_app/lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:budget_ui_app/data/data.dart';
+import 'package:budget_ui_app/models/expense_model.dart';
 import 'package:budget_ui_app/widgets/bar_chart.dart';
 import 'package:flutter/material.dart';
+
+import '../models/category_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +13,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Container _buildCategory(Category category, double totalAmount) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      height: 100.0,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0, 2),
+            blurRadius: 6.0,
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                category.name,
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                '\$${(category.maxAmount - totalAmount).toStringAsFixed(2)}/ \$${category.maxAmount}',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +62,11 @@ class _HomeScreenState extends State<HomeScreen> {
             expandedHeight: 100.0,
             backgroundColor: Theme.of(context).primaryColor,
             centerTitle: true,
-            leading: IconButton(icon: Icon(Icons.settings), iconSize: 30.0, onPressed: (){},),
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              iconSize: 30.0,
+              onPressed: () {},
+            ),
             flexibleSpace: FlexibleSpaceBar(
               centerTitle: true,
               title: Text(
@@ -35,27 +79,44 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             actions: <Widget>[
-              IconButton(icon: Icon(Icons.add), iconSize: 30.0, onPressed: (){},)
+              IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                onPressed: () {},
+              ),
             ],
           ),
           SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index){
+            delegate: SliverChildBuilderDelegate((
+              BuildContext context,
+              int index,
+            ) {
+              if (index == 0) {
                 return Container(
-                  margin: EdgeInsets.all(10.0),
-
+                  margin: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
 
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    boxShadow: [BoxShadow(color: Colors.black12, offset: Offset(0,2), blurRadius: 6.0)],
-                    borderRadius: BorderRadius.circular(10.0)
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(0, 2),
+                        blurRadius: 6.0,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                  child: BarChart(weeklySpending)
+                  child: BarChart(weeklySpending),
                 );
+              } else {
+                final Category category = categories[index - 1];
+                double totalAmountSpent = 0;
+                category.expenses.forEach((Expense expense) {
+                  totalAmountSpent += expense.cost;
+                });
+                return _buildCategory(category, totalAmountSpent);
               }
-              ,
-              childCount: 1,
-            ),
+            }, childCount: 1 + categories.length),
           ),
         ],
       ),
