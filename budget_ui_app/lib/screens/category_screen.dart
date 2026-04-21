22e747e255b09910b12b9522094d85216168cf38
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-04-21 14:50:37 +0630

    custom painter radial progress bar

--- budget_ui_app/lib/screens/category_screen.dart
+++ budget_ui_app/lib/screens/category_screen.dart
@@ -1,6 +1,10 @@
 import 'package:budget_ui_app/models/category_model.dart';
 import 'package:flutter/material.dart';
 
+import '../helpers/color_helper.dart';
+import '../models/expense_model.dart';
+import '../widgets/radial_painter.dart';
+
 class CategoryScreen extends StatefulWidget {
   final Category category;
 
@@ -13,6 +17,14 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    double totalAmountSpent = 0;
+    widget.category.expenses.forEach((Expense expense) {
+      totalAmountSpent += expense.cost;
+    });
+
+    final double amountLeft = widget.category.maxAmount - totalAmountSpent;
+    final double percent = amountLeft / widget.category.maxAmount;
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -48,14 +60,34 @@ class _CategoryScreenState extends State<CategoryScreen> {
               height: 250.0,
               width: double.infinity,
               decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: [BoxShadow(color: Colors.black12, offset: Offset(0,2), blurRadius: 6.0)]
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 2),
+                    blurRadius: 6.0,
+                  ),
+                ],
               ),
-
-            )
+              child: CustomPaint(
+                foregroundPainter: RadialPainter(
+                  bgColor: Colors.grey[200]!,
+                  lineColor: getColor(context, percent),
+                  percent: percent,
+                  width: 15.0,
+                ),
+                child: Center(
+                  child: Text(
+                    '\$${amountLeft.toStringAsFixed(2)} / \$${widget.category.maxAmount.toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
-      )
+      ),
     );
   }
 }
